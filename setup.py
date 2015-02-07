e920654bda1596d02705b6206ff66a5328be7ab6
daiki_shimada_mba <sheema.sheemap@gmail.com>
2015-02-08 06:53:29 +0900

    add setup.py On branch master Changes to be committed: 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from patolabor-python import __author__, __version__, __license__
+
+setup(
+	name = 'patolabor-python',
+	version = __version__,
+	description = 'Python module for observing file events',
+	license = __license__,
+	author = __author__,
+	author_email = 'sheema.sheemap@gmail.com',
+	url = 'https://github.com/DaikiShimada/patolabor-python.git',
+	keywords = 'filewatch filevent file monitoring python',
+	packages = find_packages(),
+	install_requires = [],
+)
