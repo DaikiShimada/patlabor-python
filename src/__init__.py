015bbb8235db54254b616a3d2374e2813a648a9c
daiki_shimada_mba <sheema.sheemap@gmail.com>
2015-02-08 05:42:17 +0900

    add __init__.py On branch master Changes to be committed: 	new file:   __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Patolabor-python
+# version 0.0.1
+#
+# Copyright (c) 2015 Daiki SHIMADA
+#
+# Licensed under the MIT License;
+# you may NOT use this module except in compliance with the License.
+# You may see a copy of the License at
+#
+#	http://opensource.org/licenses/mit-license.php	
+#
+
+__author__  = 'Daiki SHIMADA'
+__version__ = '0.0.1'
+__license__ = 'MIT'
